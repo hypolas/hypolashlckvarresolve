58e8a272f624d75fc6a558fa5c16a64f8f04e484
hypolas <nicolas.hypolite@gmail.com>
2023-03-04 17:04:21 +0100

    Print log in test

--- log_test.go
+++ log_test.go
@@ -1,6 +1,9 @@
 package hypolashlckvarresolve
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"testing"
 )
 
@@ -11,9 +14,29 @@ var (
 	}
 )
 
+const (
+	testPath = "test/log.txt"
+)
+
 // TestResolve test different strings format
 func TestResolve(t *testing.T) {
+	logf = makeLogger(testPath)
 	for _, str := range stringList {
 		ResolveVariable(str)
 	}
+
+	readFile, err := os.Open(logPath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
 }
